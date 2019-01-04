160530ddf0c9cd1759e7bab03c3ae6835b85d297
Andrei <drang.andray@gmail.com>
2019-01-05 02:57:04 +0300

    Refactor `hamming_distance`

--- hamming_distance.py
+++ hamming_distance.py
@@ -4,4 +4,4 @@ def hamming_distance(first_hash: str, second_hash: str)->int:
     """
     if len(first_hash) != len(second_hash):
         raise ValueError("Undefined for sequences of unequal length")
-    return sum(el1 != el2 for el1, el2 in zip(first_hash, second_hash))
+    return sum(element_1 != element_2 for element_1, element_2 in zip(first_hash, second_hash))
