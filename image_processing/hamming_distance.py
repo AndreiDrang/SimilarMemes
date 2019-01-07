89e3425084d0301b7dfb95b1dbd328460dcbc172
Andrei <drang.andray@gmail.com>
2019-01-07 19:21:37 +0300

    Update hamming_distance counting function

--- image_processing/hamming_distance.py
+++ image_processing/hamming_distance.py
@@ -1,30 +1,35 @@
 import collections
 import itertools
 
-import numpy as np
 
-def hamming_distance(images_dict: collections.defaultdict)->collections.deque:
+def hamming_distance(hashes_list: list)->collections.deque:
     """
     Return the Hamming distance between equal-length sequences
 
-    :param images_dict: Dict with full image info
+    :param hashes_list: List contains hashes list:
+                            0 - dhash
+                            1 - md5 unique hash
 
     :return: List of pair with hamming distance param
     """
     # make unique photo files combinations
-    images_pairs = itertools.combinations(images_dict, 2)
+    images_pairs = itertools.combinations(hashes_list, 2)
 
     hamming_pairs = collections.deque()
     
     for pair in images_pairs:
         # get hash values for images in pair
-        first_hash = images_dict[pair[0]]['dhash']
-        second_hash = images_dict[pair[1]]['dhash']
+        first_hash = pair[0][0]
+        second_hash = pair[1][0]
 
         if len(first_hash) != len(second_hash):
             print("Undefined for sequences of unequal length")
         else:
-            # get hash different
-            hamming_pairs.append((pair, sum(element_1 != element_2 for element_1, element_2 in zip(first_hash, second_hash))))
+            # count dhash different
+            diff = sum(element_1 != element_2 for element_1, element_2 in zip(first_hash, second_hash))
+            # if dhahs different < 10 - images ar similar
+            if diff<10:
+                # get hash different
+                hamming_pairs.append((pair, diff))
 
     return hamming_pairs
