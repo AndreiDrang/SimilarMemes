d75aa060caebb7469af85f1128208cdd3af6c8d1
Andrei <drang.andray@gmail.com>
2019-01-09 05:06:59 +0300

    Add new models - Video, ImageTag, VideoTag, ImageDuplicates, VideoDuplicates

--- database/__init__.py
+++ database/__init__.py
@@ -1,1 +1,2 @@
-from .models import Image
+from .models import Image, ImageTag, Video, ImageDuplicates, db_session, db
+from .db_methods import save_new_files, save_images_duplicates
--- database/models.py
+++ database/models.py
@@ -1,37 +1,116 @@
+import collections
 from datetime import datetime
 
 from pony.orm import *
 
 db = Database()
 
-db.bind(provider='sqlite', filename='./database.sqlite', create_db=True)
-
+db.bind(provider='postgres', 
+        user='vkbot_admin', 
+        password='vkbot_pass', 
+        #host='80.211.243.157', 
+        host='localhost',
+        database='vkbot')
 
 
 class Image(db.Entity):
     """
     Image model
     """
+
+    _table_ = 'Image'
+
     # image path
     image_path = Required(str, unique=True)
+    # image name
+    image_name = Required(str)
     # image dhash
     image_dhash = Required(str)
     # image md5 hash
-    image_md5_hash = Required(str, unique=True)
+    image_md5_hash = Required(str)
     # image creation datetime(in DB)
     image_creation = Required(datetime, default=datetime.now)
     # image tags
-    #image_tags = relationship('Tag', secondary=association_table, backref="images")
-        
+    image_tags = Set('ImageTag', nullable=True)
+    # image duplicates
+    image_duplicates = Set('ImageDuplicates', nullable=True)
+
+    composite_key(image_path, image_md5_hash)
+
     @staticmethod
-    @db_session   
-    def insert_new(image_path, image_dhash, image_md5_hash):
-        if Image.get(image_md5_hash=image_md5_hash):
-            return False
-        else:
-            Image(image_path=image_path,
-                  image_dhash=image_dhash,
-                  image_md5_hash=image_md5_hash)
-            return True
+    @db_session(retry=3)
+    def get_dhash_id()->core.Query:
+        return collections.deque(select((image.image_dhash, image.id) for image in Image)[:])
+
+class Video(db.Entity):
+    """
+    Video model
+    """
+
+    _table_ = 'Video'
+
+    # video path
+    video_path = Required(str, unique=True)
+    # video name
+    video_name = Required(str)
+    # video tags
+    video_tags = Set('VideoTag', nullable=True)
+    # video duplicates
+    video_duplicates = Set('VideoDuplicates', nullable=True)
+
+
+class ImageTag(db.Entity):
+    """
+    Image tag model
+    """
+
+    _table_ = 'Image_Tag'
+    
+    # image tags
+    tag_text = Required(str, max_len=40)
+    # tag images list
+    tag_images = Set(Image, nullable=True)
+
+
+class VideoTag(db.Entity):
+    """
+    Video tag model
+    """
+
+    _table_ = 'Video_Tag'
+    
+    # image tags
+    tag_text = Required(str, max_len=40)
+    # tag video list
+    tag_video = Set(Video, nullable=True)
+
+
+class ImageDuplicates(db.Entity):
+    """
+    Duplicates model
+    """
+
+    _table_ = 'Images_Duplicates'
+    
+    # bounded image id
+    image_hamming_id = Required(int)
+    # hamming distance param
+    images_hamming_distance = Required(int)
+    # images duplicates
+    image_duplicates = Set(Image)
+
+
+class VideoDuplicates(db.Entity):
+    """
+    Duplicates model
+    """
+
+    _table_ = 'Video_Duplicates'
+    
+    # video duplicates
+    video_duplicates = Set(Video)
+
 
 db.generate_mapping(create_tables=True)
+
+print("Все таблицы в БД успешно созданы")
