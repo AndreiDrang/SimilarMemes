369bb65c8e645c0e11974b3d469ac14209d95b50
Andrei <drang.andray@gmail.com>
2019-01-21 23:29:09 +0300

    Add video analyze for tests

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ from database import save_new_files, Image, save_images_duplicates
 
 
 
-path = '/home/andrei/Downloads/Telegram Desktop/DataExport_06_12_2018/chats/chat_001/photos'
-#path = '/home/andrei/Pictures'
+#path = '/home/andrei/Downloads/Telegram Desktop/DataExport_06_12_2018/chats/chat_001/photos'
+path = '/home/andrei/Pictures'
 
 
 
@@ -26,25 +26,29 @@ print(time.time()-start_time)
 # get photo and video files lists
 image_files_list, video_files_list = index_folder_files(files_paths=files_paths)
 
-print('Images indexed')
+#print('Images indexed')
 print(time.time()-start_time)
+
+print(video_files_list)
+
+video_processing(video_files_list)
 
 # parse images and get dict
-result_image_dict = image_processing(image_files_list)
-print(len(result_image_dict),' - Images processed.')
+#result_image_dict = image_processing(image_files_list)
+#print(len(result_image_dict),' - Images processed.')
 
-print(time.time()-start_time)
+#print(time.time()-start_time)
 
 # save all images to DB
-save_new_files(result_image_dict, 'image')
+#save_new_files(result_image_dict, 'image')
 
 
 image_files_query = Image.get_dhash_id()
 
 # count hamming_distance betwen images hash
-hamming_pairs = hamming_distance(hashes_list=image_files_query)
-print(len(hamming_pairs))
-save_images_duplicates(hamming_pairs=hamming_pairs)
+#hamming_pairs = hamming_distance(hashes_list=image_files_query)
+#print(len(hamming_pairs))
+#save_images_duplicates(hamming_pairs=hamming_pairs)
 
-print(f'Needed time for {len(result_image_dict)} images - {time.time()-start_time}')
+#print(f'Needed time for {len(result_image_dict)} images - {time.time()-start_time}')
 
