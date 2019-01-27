d50d1187bd7d11f5aea49e3e32a1bcb3906d109b
Andrei <drang.andray@gmail.com>
2019-01-27 23:45:16 +0300

    Prepare audio_processing script

--- video_processing/audio_processing.py
+++ video_processing/audio_processing.py
@@ -1,6 +1,6 @@
-from moviepy.audio.io.AudioFileClip import AudioFileClip
 
-def audio_processing(audio_clip: AudioFileClip):
+
+def audio_processing(audio_clip):
     """
     Get audio file processing it and try find same in local DB
     """
