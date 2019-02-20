a3eaef6516e5cbb5969c8750b8b1d4087e80130e
Andrei <drang.andray@gmail.com>
2019-02-20 19:11:53 +0300

    Add feature_description example to main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import time
 
 from indexing import index_folder_files, folders_files_walk
-from image_processing import hamming_distance, image_processing
+from image_processing import hamming_distance, image_processing, feature_description
 from video_processing import video_processing
 from database import save_new_files, Image, save_images_duplicates
 
@@ -9,7 +9,7 @@ from database import save_new_files, Image, save_images_duplicates
 
 
 #path = '/home/andrei/Downloads/Telegram Desktop/DataExport_06_12_2018/chats/chat_001/photos'
-path = '/home/andrei/Pictures'
+path = '/home/andrey.rachalovsky/Pictures'
 
 
 
@@ -24,14 +24,16 @@ print(time.time()-start_time)
 image_files_list, video_files_list = index_folder_files(files_paths=files_paths)
 
 #print('Images indexed')
+
+
+result_image_features_list = feature_description(images_list=image_files_list)
+print(result_image_features_list)
 print(time.time()-start_time)
-
-print(video_files_list)
-
-video_processing(video_files_list)
+time.sleep(100)
+result_video_dict = video_processing(video_files_list)
 
 # parse images and get dict
-#result_image_dict = image_processing(image_files_list)
+result_image_dict = image_processing(image_files_list)
 #print(len(result_image_dict),' - Images processed.')
 
 #print(time.time()-start_time)
@@ -40,7 +42,7 @@ video_processing(video_files_list)
 #save_new_files(result_image_dict, 'image')
 
 
-image_files_query = Image.get_dhash_id()
+#image_files_query = Image.get_dhash_id()
 
 # count hamming_distance betwen images hash
 #hamming_pairs = hamming_distance(hashes_list=image_files_query)
