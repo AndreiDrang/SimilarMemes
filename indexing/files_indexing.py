4663d92355e71323e2097cda7cee33838f6144de
Andrei <drang.andray@gmail.com>
2019-03-13 20:10:13 +0300

    Update index_folder_files - add logic from `folders_files_walk`. Add `get_depth` function
    Add param to parse certain file formats - images or videos (or both of them)

--- indexing/files_indexing.py
+++ indexing/files_indexing.py
@@ -25,34 +25,48 @@ def is_video(file_name: str)->bool:
     else:
         return False
 
+def get_depth(start_path: str, end_path: str):
+    """
+    Get start path and current path - count current path walk depth
+    """
+    depth = len(end_path.split('/'))-len(start_path.split('/'))
+    
+    return depth
 
-def index_folder_files(files_paths: collections.deque)->(collections.deque, collections.deque):
+def index_folder_files(path: collections.deque, max_depth: int = 3, indexing_type: str = 'all')->(collections.deque, collections.deque):
     """
     Function indexing image/video files in folder
 
-    :param files_paths: List of full files path adresses
-                            0 - file name
-                            1 - file full path
+    :param path: Folder full path
+    :param max_depth: Folders walking max depth
+    :param indexing_type: File type to index; Available params - `image` / `video` / `all`;
 
     :return: List of two lists: image files and video files
                 0 - file name
                 1 - file full path
     """
+    # create folders tree
+    tree = os.walk(path)
 
     # prepare video and photo files lists
     image_files_list = collections.deque()
     video_files_list = collections.deque()
 
-    for file_ in files_paths:
-        # check if file - image
-        if is_image(file_[0]):
-            # add to image list
-            image_files_list.append(file_)
+    # looping throught tree
+    for data in tree:
+        # if max depth not reached
+        if get_depth(path, data[0])<=max_depth:
+            for file_ in data[2]:
+                # check if file - image
+                if is_image(file_) and indexing_type in ('image', 'all'):
+                    # add to image list
+                    image_files_list.append((file_, data[0]))
 
-        # if file - video
-        elif is_video(file_[0]):
-            # add to video list
-            video_files_list.append(file_)
-
+                # if file - video
+                elif is_video(file_) and indexing_type in ('video', 'all'):
+                    # add to video list
+                    video_files_list.append((file_, data[0]))
+        else:
+            break
 
     return image_files_list, video_files_list
