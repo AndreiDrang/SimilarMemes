7e1590c1a0ed8682e5f475c985cc92492770e5fa
Andrei <drang.andray@gmail.com>
2019-03-18 22:51:59 +0300

    Delete hamming distance method and update main example

--- image_processing/__init__.py
+++ image_processing/__init__.py
@@ -1,3 +1,2 @@
-from .hamming_distance import hamming_distance
 from .image_processing import image_processing
 from .feature_description import feature_description
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import time
 
 from indexing import index_folder_files
-from image_processing import image_processing, feature_description, hamming_distance
+from image_processing import image_processing, feature_description
 from video_processing import video_processing
 from database import save_new_files, Image, save_images_duplicates
 
@@ -25,37 +25,33 @@ image_files_list, video_files_list = index_folder_files(
                                             indexing_type='all'
                                         )
 
-print(image_files_list)
-print(video_files_list)
+#print(image_files_list)
+#print(video_files_list)
 
 print('Files indexed')
 
 
-result_image_features_list = feature_description(images_list=image_files_list)
-print(result_image_features_list)
-
-print(time.time()-start_time)
-
-result_video_dict = video_processing(video_files_list)
-print(result_video_dict)
+#result_video_dict = video_processing(video_files_list)
+#print(result_video_dict)
 result_image_dict = image_processing(image_files_list)
-print(result_image_dict)
-
-# parse images and get dict
-#print(len(result_image_dict),' - Images processed.')
-
-#print(time.time()-start_time)
+#print(result_image_dict)
 
 # save all images to DB
-#save_new_files(result_image_dict, 'image')
+save_new_files(result_image_dict, 'image')
 
 
-#image_files_query = Image.get_dhash_id()
+image_files_query = Image.get_files_paths()
 
-# count hamming_distance betwen images hash
-#hamming_pairs = hamming_distance(hashes_list=image_files_query)
-#print(len(hamming_pairs))
-#save_images_duplicates(hamming_pairs=hamming_pairs)
+
+result_image_features_list = feature_description(images_list=image_files_query)
+
+print(result_image_features_list)
+print(time.time()-start_time)
+
+save_images_duplicates(pairs=result_image_features_list)
 
 #print(f'Needed time for {len(result_image_dict)} images - {time.time()-start_time}')
 
+print(len(image_files_list))
+print(len(result_image_dict))
+print(len(image_files_query))
