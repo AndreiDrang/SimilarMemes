5e601504ba510b76e7fd50fb54197a63774727ec
Andrei <drang.andray@gmail.com>
2019-03-18 22:53:30 +0300

    Update image processing - delete path name as dict key(add enumerate); Delete imagehash library

--- image_processing/image_processing.py
+++ image_processing/image_processing.py
@@ -2,13 +2,12 @@ import collections
 import hashlib
 import os
 
-import imagehash
 from PIL import Image as PIL_Img
 
 
 def image_processing(image_list: collections.deque)->collections.defaultdict:
     """
-    Function preprocess image files, count hash, dhash and return files info in dict format
+    Function preprocess image files, count hash and return files info in dict format
 
     :param image_list: List of images in folder
                         0 - files name
@@ -19,20 +18,17 @@ def image_processing(image_list: collections.deque)->collections.defaultdict:
     # prepare result dict
     result_image_dict = collections.defaultdict()
 
-    for image_file in image_list:
+    for index, image_file in enumerate(image_list):
         # open image
         img = PIL_Img.open(image_file[1]+os.sep+image_file[0])
         # resize image to 20*19 format
         img.thumbnail(size=(9,8))
         img.convert('LA')
-        # get image hash
-        hash_result = str(imagehash.dhash(img))
 
         result_image_dict.update({
-                                    image_file[0]: {
+                                    index: {
                                         'namepath': image_file,
-                                        'dhash': hash_result,
-                                        'md5_hash': hashlib.md5(image_file[0].encode()).hexdigest(),
+                                        'md5_hash': hashlib.md5((image_file[1]+os.sep+image_file[0]).encode()).hexdigest(),
                                     }
                                 })
     
