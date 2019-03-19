a1075b70332630b8ff91c3e37afe169e56bf474d
PetPython <42939994+PetPython@users.noreply.github.com>
2019-03-19 21:57:29 +0300

    Update settings.py
    moar extensions

--- indexing/settings.py
+++ indexing/settings.py
@@ -1,6 +1,6 @@
 # list of available images formats
-IMAGE_FORMATS = (".png", ".jpg", ".jpeg", ".bmp", ".jpe", ".dib", ".gif")
+IMAGE_FORMATS = (".png", ".jpg", ".jpeg", ".bmp", ".jpe", ".dib", ".gif", ".ico")
 # list of available video formats
-VIDEO_FORMATS = (".mp4", ".webm", ".mov")
+VIDEO_FORMATS = (".mp4", ".webm", ".mov", ".avi", ".wmv", ".3gp", ".mkv", ".flv")
 # precompiled regex to get file extension
 FILE_EXTENSION_RE = "([.]\w{3,4}$)"
