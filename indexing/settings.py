5cf82359f4ae2322905bad311dccdff04b5e6a3f
Andrei <drang.andray@gmail.com>
2019-03-19 21:53:39 +0300

    Add pyqt to requirement; Add `mov` to video format

--- indexing/settings.py
+++ indexing/settings.py
@@ -1,6 +1,6 @@
 # list of available images formats
 IMAGE_FORMATS = (".png", ".jpg", ".jpeg", ".bmp", '.jpe', '.dib', '.gif')
 # list of available video formats
-VIDEO_FORMATS = (".mp4", ".webm")
+VIDEO_FORMATS = (".mp4", ".webm", ".mov")
 # precompiled regex to get file extension
 FILE_EXTENSION_RE = '([.]\w{3,4}$)'
