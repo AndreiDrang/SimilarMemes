937596bf0f56f3f22097344cb1ba68398a4e5d42
Andrei <drang.andray@gmail.com>
2019-03-30 03:13:59 +0300

    Delete useless image formats

--- indexing/settings.py
+++ indexing/settings.py
@@ -1,5 +1,5 @@
 # list of available images formats
-IMAGE_FORMATS = (".png", ".jpg", ".jpeg", ".bmp", ".jpe", ".dib", ".gif", ".ico")
+IMAGE_FORMATS = (".png", ".jpg", ".jpeg", ".bmp", ".jpe")
 # list of available video formats
 VIDEO_FORMATS = (".mp4", ".webm", ".mov", ".avi", ".wmv", ".3gp", ".mkv", ".flv")
 # precompiled regex to get file extension
