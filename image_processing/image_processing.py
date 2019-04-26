7318f30a3ea706138c066c16cf2b6b0c194ed9cf
Andrei <drang.andray@gmail.com>
2019-04-27 00:12:08 +0300

    Update image processing function - start counting orb_descriptor

--- image_processing/image_processing.py
+++ image_processing/image_processing.py
@@ -2,12 +2,15 @@ import collections
 import hashlib
 import os
 
-from PIL import Image as PIL_Img
+import cv2
+import imageio
+import numpy as np
 
 
 def image_processing(image_list: collections.deque) -> collections.defaultdict:
     """
-    Function preprocess image files, count hash and return files info in dict format
+    Function preprocess image files, count hash, count images descriptor(using ORB) 
+        and return files info in dict format
 
     :param image_list: List of images in folder
                         0 - files name
@@ -17,23 +20,36 @@ def image_processing(image_list: collections.deque) -> collections.defaultdict:
     """
     # prepare result dict
     result_image_dict = collections.defaultdict()
+    # prepare ORB to count images descriptor
+    orb = cv2.ORB_create()
 
     for index, image_file in enumerate(image_list):
-        # open image
-        img = PIL_Img.open(image_file[1] + os.sep + image_file[0])
-        # resize image to 20*19 format
-        img.thumbnail(size=(9, 8))
-        img.convert("LA")
+        
+        # if image is GIF
+        if image_file[0][-3:].lower()=='gif':
+            # read gif
+            gif = imageio.mimread(image_file[1] + os.sep + image_file[0])
+            # get middle frame from gif
+            image = gif[len(gif)//2]
+        else:
+            image = cv2.imread(image_file[1] + os.sep + image_file[0], 0)
 
-        result_image_dict.update(
-            {
-                index: {
-                    "namepath": image_file,
-                    "md5_hash": hashlib.md5(
-                        (image_file[1] + os.sep + image_file[0]).encode()
-                    ).hexdigest(),
+        # count image descriptor        
+        _, orb_descriptor = orb.detectAndCompute(image, None)
+        
+        # check if orb_descriptor is counted
+        if type(orb_descriptor)==np.ndarray:
+            # write result to dict
+            result_image_dict.update(
+                {
+                    index: {
+                        "namepath": image_file,
+                        "orb_descriptor": orb_descriptor.tobytes(),
+                        "md5_hash": hashlib.md5(
+                            (image_file[1] + os.sep + image_file[0]).encode()
+                        ).hexdigest(),
+                    }
                 }
-            }
-        )
+            )
 
     return result_image_dict
