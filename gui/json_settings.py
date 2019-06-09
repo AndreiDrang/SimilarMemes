1f856e73b38e2910e4888e01480c63eb7f6380ff
Andrei <drang.andray@gmail.com>
2019-06-10 02:51:16 +0300

    Add db_config_json reader to gui

--- gui/json_settings.py
+++ gui/json_settings.py
@@ -4,7 +4,7 @@ from PyQt5.QtWidgets import *
 
 
 # Reads the json settings file and returns a needed setting value:
-def json_read(settingName: str):
+def user_json_read(settingName: str):
     try:
         with open(f"gui{os.sep}user_settings.json", "r") as jsonFile:
             jsonData = json.load(jsonFile)
@@ -15,7 +15,7 @@ def json_read(settingName: str):
 
 
 # Updates a needed setting and rewrites the json settings file:
-def json_update(settingName: str, settingNewValue: int):
+def user_json_update(settingName: str, settingNewValue: int):
     try:
         with open(f"gui{os.sep}user_settings.json", "r") as jsonFile:
             jsonData = json.load(jsonFile)
@@ -23,6 +23,32 @@ def json_update(settingName: str, settingNewValue: int):
         jsonData[settingName] = int(settingNewValue)
 
         with open(f"gui{os.sep}user_settings.json", "w") as jsonFile:
+            json.dump(jsonData, jsonFile)
+
+    except Exception as ex:
+        warning_message(ex)
+
+
+# Reads the json settings file and returns a needed setting value:
+def db_json_read(settingName: str):
+    try:
+        with open(f"database{os.sep}db_config.json", "r") as jsonFile:
+            jsonData = json.load(jsonFile)
+        return jsonData[settingName]
+
+    except Exception as ex:
+        warning_message(ex)
+
+
+# Updates a needed setting and rewrites the json settings file:
+def db_json_update(settingName: str, settingNewValue: str):
+    try:
+        with open(f"database{os.sep}db_config.json", "rt") as jsonFile:
+            jsonData = json.load(jsonFile)
+
+        jsonData[settingName] = settingNewValue
+
+        with open(f"database{os.sep}db_config.json", "wt") as jsonFile:
             json.dump(jsonData, jsonFile)
 
     except Exception as ex:
