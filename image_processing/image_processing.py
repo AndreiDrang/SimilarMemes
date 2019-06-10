45e015e04d401f1026437b50dc0d1a9155e9178a
Andrei <drang.andray@gmail.com>
2019-06-10 18:17:12 +0300

    Add try/except to image count_descriptor function

--- image_processing/image_processing.py
+++ image_processing/image_processing.py
@@ -11,32 +11,35 @@ from database import save_new_files
 
 
 def count_descriptor(image_file):
-    orb = cv2.ORB_create()
+    try:
+        orb = cv2.ORB_create()
 
-    # if image is GIF
-    if image_file[0][-3:].lower() == "gif":
-        # read gif
-        gif = imageio.mimread(image_file[1] + os.sep + image_file[0])
-        # get middle frame from gif
-        image = gif[len(gif) // 2]
-    else:
-        image = cv2.imread(image_file[1] + os.sep + image_file[0], 0)
+        # if image is GIF
+        if image_file[0][-3:].lower() == "gif":
+            # read gif
+            gif = imageio.mimread(image_file[1] + os.sep + image_file[0])
+            # get middle frame from gif
+            image = gif[len(gif) // 2]
+        else:
+            image = cv2.imread(image_file[1] + os.sep + image_file[0], 0)
 
-    # count image descriptor
-    _, orb_descriptor = orb.detectAndCompute(image, None)
+        # count image descriptor
+        _, orb_descriptor = orb.detectAndCompute(image, None)
 
-    # check if orb_descriptor is counted
-    if type(orb_descriptor) == np.ndarray:
-        # if orb_descriptor find many points - save them
-        if orb_descriptor.shape[0] > 2:
-            return {
-                "namepath": image_file,
-                "orb_descriptor": orb_descriptor.tobytes(),
-                "md5_hash": hashlib.md5(
-                    (image_file[1] + os.sep + image_file[0]).encode()
-                ).hexdigest(),
-            }
-    return None
+        # check if orb_descriptor is counted
+        if type(orb_descriptor) == np.ndarray:
+            # if orb_descriptor find many points - save them
+            if orb_descriptor.shape[0] > 2:
+                return {
+                    "namepath": image_file,
+                    "orb_descriptor": orb_descriptor.tobytes(),
+                    "md5_hash": hashlib.md5(
+                        (image_file[1] + os.sep + image_file[0]).encode()
+                    ).hexdigest(),
+                }
+    except:
+        print(traceback.format_exc())
+        return None
 
 
 def image_processing(image_list: collections.deque):
