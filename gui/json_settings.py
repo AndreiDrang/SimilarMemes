861c1b671c04676a82edb04b85faafd362e8ebab
Andrei <drang.andray@gmail.com>
2019-06-17 20:40:11 +0300

    Add to json_settings `image_processing` module settings

--- gui/json_settings.py
+++ gui/json_settings.py
@@ -55,6 +55,32 @@ def db_json_update(settingName: str, settingNewValue: str):
         warning_message(ex)
 
 
+# Reads the json settings file and returns a needed setting value:
+def processing_json_read(settingName: str):
+    try:
+        with open(f"database{os.sep}settings.json", "r") as jsonFile:
+            jsonData = json.load(jsonFile)
+        return jsonData[settingName]
+
+    except Exception as ex:
+        warning_message(ex)
+
+
+# Updates a needed setting and rewrites the json settings file:
+def processing_json_update(settingName: str, settingNewValue: str):
+    try:
+        with open(f"database{os.sep}settings.json", "rt") as jsonFile:
+            jsonData = json.load(jsonFile)
+
+        jsonData[settingName] = settingNewValue
+
+        with open(f"database{os.sep}settings.json", "wt") as jsonFile:
+            json.dump(jsonData, jsonFile)
+
+    except Exception as ex:
+        warning_message(ex)
+
+
 # Shows a dialog if an error occurs (e.g., if the file is missing):
 def warning_message(ex):
     warning = QMessageBox()
