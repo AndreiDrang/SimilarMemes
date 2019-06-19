79c1f5c26a4b993bc8909e777eb6d77e1e14df5f
Andrei <drang.andray@gmail.com>
2019-06-19 09:48:40 +0300

    Fix windows GUI run

--- run.py
+++ run.py
@@ -3,11 +3,14 @@ import sys
 from gui.main import QApplication, MainWindow
 import database
 
-# connect to DB
-database.connection()
 
-# start GUI
-app = QApplication(sys.argv)
-screen = MainWindow()
-screen.show()
-sys.exit(app.exec_())
+if __name__=='__main__':
+
+    # connect to DB
+    database.connection()
+
+    # start GUI
+    app = QApplication(sys.argv)
+    screen = MainWindow()
+    screen.show()
+    sys.exit(app.exec_())
